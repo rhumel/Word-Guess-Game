79f39dc3b538049d2b9f7ed9b14e100c69ea8a2c
rhumel <rhumel@hotmail.com>
2018-06-15 21:21:40 -0400

    Add reset function to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,114 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wordBank = ["sofa","sunglasses", "lotion", "sunscreen", "beach", "water", "sand","book", "pencil","ruler","plant","grandma", "picture", "drum", "chair", "wood", "carpet", "fireplace", "jar", "trophy", "candle", "treasure", "glitter", "paper", "spray", "dishes", "broom", "soap", "camera", "speaker", "rainbow", "twilight", "star", "moon", "pies", "cake", "diamonds", "butterflies","caterpiller","tree","driveway", "front", "family","hotel", "vacation", "shoe", "pool", "table", "fabric", "medicine", "commercial","automobile", "truck", "bandage","hospital","strecher", "doctor", "nurse", "sick","blanket", "studio", "loan", "lion", "tiger", "bear", "computer", "javascript", "frame", "billboard","interview", "blind", "landscape", "pregnant", "cabinet", "drawer", "pumpkin","investigation","boat","graze", "tractor", "trailer", "sailor", "microwave", "centipede", "nerve", "surgery", "laser", "institute", "download", "beautiful", "truth", "moment", "record", "cracked", "interrogation", "invasion"]
-var word ;              // Selected word
-var guess ;             // Geuss
-var guessedLetters = [ ]; // Stored geusses array
-var lives = 6 ;             // Lives
-var counter = 0 ;           // Count correct guesses
+var wordBank = ["sofa", "sunglasses", "lotion", "sunscreen", "beach", "water", "sand", "book", "pencil", "ruler", "plant", "grandma", "picture", "drum", "chair", "wood", "carpet", "fireplace", "jar", "trophy", "candle", "treasure", "glitter", "paper", "spray", "dishes", "broom", "soap", "camera", "speaker", "rainbow", "twilight", "star", "moon", "pies", "cake", "diamonds", "butterflies", "caterpiller", "tree", "driveway", "front", "family", "hotel", "vacation", "shoe", "pool", "table", "fabric", "medicine", "commercial", "automobile", "truck", "bandage", "hospital", "strecher", "doctor", "nurse", "sick", "blanket", "studio", "loan", "lion", "tiger", "bear", "computer", "javascript", "frame", "billboard", "interview", "blind", "landscape", "pregnant", "cabinet", "drawer", "pumpkin", "investigation", "boat", "graze", "tractor", "trailer", "sailor", "microwave", "centipede", "nerve", "surgery", "laser", "institute", "download", "beautiful", "truth", "moment", "record", "cracked", "interrogation", "invasion"]
+var word;              // Selected word
+var guess;             // Geuss
+var guessedLetters = []; // Stored geusses array
+var lives = 6;             // Lives
+var counter = 0;           // Count correct guesses
 var spaces;              // Number of spaces in word '-'
 var wrongLetters = [];   //wrong guess 
-var wins = 0 ;             //wins
-var losses = 0 ;           //loses 
+var wins = 0;             //wins
+var losses = 0;           //loses 
+var letterExist = false;  //check to see 
 
 //Select random word from word bank
 
-var word = wordBank[Math.floor(Math.random()*wordBank.length)];
-console.log(word);
+function resestGame() {
+    word = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(word);
 
-//get the length of word
-var spaces = word.length;
-console.log(spaces);
+    //get the length of word
+    //  spaces = word.length;
+    //  console.log(spaces);
+    lives = 6;
+    counter = 0;
+    guessedLetters = []
+    wrongLetters = []
 
-//create "_" blanks 
-for (var i =0; i <word.length; i++) {
-    guessedLetters.push("_");
-    
-}
-console.log(guessedLetters);
-
-//collect guess from user
-
-var guess = document.getElementById("letterTxt") 
-    document.onkeyup=function(event) {
-    guess = event.key.toLowerCase();
-   
-    console.log("first guess " + guess);
-    console.log(event);
-        for (var i = 0; i <= word.length-1; i++) {
-            console.log("this is the index " + i);
-            console.log("this is the guess " + guess);
-            console.log("this is the word-char " + word.charAt(i));
-        if (word.charAt(i) === guess) {
-            guessedLetters[i] = guess;
-            console.log(guessedLetters);
-           var counter = (counter + 1);       
-           
-        }  else { 
-           lives=lives-1;  
-           wrongLetters.push(guess);
-           console.log("wrong letter");
-           console.log(lives);
-        }
+    //create "_" blanks 
+    for (var i = 0; i < word.length; i++) {
+        guessedLetters.push("_");
 
     }
+    console.log(guessedLetters);
+
+    renderHtml();
 }
 
+resestGame();
 
-        
-   
-                
-           
+
+
+
+//collect guess from user and make it lower case
+
+//var guess = document.getElementById("letterTxt")    
+var guess;
+document.onkeyup = function (event) {
+    guess = event.key.toLowerCase();
+
+    console.log("first guess " + guess);
+    console.log(event);
+    for (var i = 0; i <= word.length - 1; i++) {
+        console.log("this is the index " + i);
+        console.log("this is the guess " + guess);
+        console.log("this is the word-char " + word.charAt(i));
+        if (word.charAt(i) === guess) {
+            guessedLetters[i] = guess;
+            counter++;
+            letterExist = true;
+
+
+            console.log(counter);
+        }
+    }
+
+
+    if (letterExist === true) {
+
+        letterExist = false
+        renderHtml();
+        console.log("i just reset the letter exist to false");
+    } else {
+        lives--;
+        wrongLetters.push(guess);
+        console.log(wrongLetters);
+        console.log(lives);
+        renderHtml();
+    }
+    if (counter === word.length) {
+        wins++;
+        resestGame();
+        //reset function
+
+        console.log(wins);
+
+    }
+    if (lives === 0) {
+
+        losses++;
+        resestGame();
+        //reset
+    }
+
+}
+function renderHtml() {
+    document.getElementById("livesTxt").innerHTML = (lives);
+    document.getElementById("winTxt").innerHTML = (wins);
+    document.getElementById("lossesTxt").innerHTML = (losses);
+    document.getElementById("wordTxt").innerHTML = (guessedLetters.join(" "));
+    document.getElementById("wrongTxt").innerHTML = (wrongLetters.join(" "));
+
+}
+document.getElementById("reset").addEventListener("click", resestGame)
+
+
+
+
+
            // do this outside of the loop
                                                                   //            if (counter === word.length) {
                                                                   //                var wins = wins +1;
@@ -78,8 +126,13 @@ var guess = document.getElementById("letterTxt")
 //                Losses = losses + 1;
 //                     console.log("added to losses");
 //            }
-    
 
 
+//else {
+       //     lives = lives - 1;
+       //     wrongLetters.push(guess);
+       //     console.log("wrong letter");
+       //     console.log(lives);
+       // }
 //}
 
